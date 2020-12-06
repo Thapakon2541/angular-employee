e7528b117297592584c601fe8e9260dbc291708d
Thapakon <36725707+Thapakon2541@users.noreply.github.com>
2020-12-06 22:54:42 +0700

    peen

---
+++ back-end/demo/src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.example.demo.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1708236615905032839L;
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+}
